8a87b3f732ee07d20c6cc28dafa4a771880dec22
iloveframedsomuchyay <95708773+iloveframedsomuchyay@users.noreply.github.com>
2023-12-01 10:05:03 -0500

    Update and rename lua.lua to calculator.lua

---
+++ calculator.lua
@@ -0,0 +1,22 @@
+function run()
+  negatives = false
+  function getsymbol()
+    randomnumber = getMathRandom(1, 4)
+      if randomnumber = 1 then
+        symbol = +
+    elseif randomnumber = 2
+      symbol = -
+    elseif randomnumber = 3
+      symbol = /
+    elseif randomnumber = 4
+      symbol = *
+  function getrandomnumber()
+        if negatives then
+          randomnumbertwo = getMathRandom(-100,100)
+          randomnumberthree = getMathRandom(-100,100)
+        else
+          randomnumbertwo = getMathRandom(0,100)
+          randomnumberthree = getMathRandom(0,100)
+  function equation()
+            
+          
