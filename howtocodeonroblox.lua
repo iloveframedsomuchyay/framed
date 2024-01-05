1b220f2433238c7b822d6f27a14028be6015a3f6
iloveframedsomuchyay <95708773+iloveframedsomuchyay@users.noreply.github.com>
2024-01-05 08:59:10 -0500

    Create howtocodeonroblox.lua

---
+++ howtocodeonroblox.lua
@@ -0,0 +1,4 @@
+-- roblox code ig
+-- i have no idea how to code on roblox so im gonna put some random stuff i heard of before
+local script.Parent.Parent.MouseButton1Click:Connect(function()
+  Workspace.FindFirstChild
